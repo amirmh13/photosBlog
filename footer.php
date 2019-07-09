005b69330fb5848ee97b5dafc5e854d928bf5ca3
AmirMohammad <amirmhaghighat@gmail.com>
2019-07-09 13:29:35 +0430

    dynamicaly theme

---
+++ footer.php
@@ -0,0 +1,115 @@
+<div class="py-3 mb-5 mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 d-md-flex align-items-center" data-aos="fade">
+            <h2 class="text-black mb-5 mb-md-0 text-center text-md-left">Need a photographer?</h2>
+            <div class="ml-auto text-center text-md-left">
+              <a href="contact.html" class="btn btn-danger py-3 px-5 rounded">Contact Me</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="mb-5">
+              <h3 class="footer-heading mb-4">About Fotograp</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque,
+                consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima
+                minus odio!</p>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <div class="row mb-5">
+              <div class="col-md-12">
+                <h3 class="footer-heading mb-4">Navigations</h3>
+              </div>
+              <div class="col-md-6 col-lg-6">
+                <ul class="list-unstyled">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Photography</a></li>
+                  <li><a href="#">Gallery</a></li>
+                  <li><a href="#">Services</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-6">
+                <ul class="list-unstyled">
+                  <li><a href="#">About Me</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Contact Me</a></li>
+                  <li><a href="#">Terms</a></li>
+                </ul>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <h3 class="footer-heading mb-4">Follow Me</h3>
+
+            <div>
+              <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+              <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+              <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+              <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+            </div>
+
+
+
+          </div>
+
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;<script data-cfasync="false"
+                src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+              <script>
+                document.write(new Date().getFullYear());
+              </script> All rights reserved | This template is made with <i class="icon-heart text-danger"
+                aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+
+
+
+  </div>
+
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery-3.3.1.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery-ui.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/popper.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/bootstrap.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/owl.carousel.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery.stellar.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery.countdown.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/bootstrap-datepicker.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/swiper.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/aos.js"></script>
+
+  <script src="<?php echo get_template_directory_uri() ?>/js/picturefill.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/lightgallery-all.min.js"></script>
+  <script src="<?php echo get_template_directory_uri() ?>/js/jquery.mousewheel.min.js"></script>
+
+  <script src="<?php echo get_template_directory_uri() ?>/js/main.js"></script>
+
+  <script>
+    $(document).ready(function () {
+      $('#lightgallery').lightGallery();
+    });
+  </script>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title><?php bloginfo('name'); wp_title(); ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300i,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/magnific-popup.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/jquery-ui.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/lightgallery.min.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap-datepicker.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/swiper.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/aos.css">
+
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
+
+
+  <?php wp_head() ?>
+
+
+</head>
