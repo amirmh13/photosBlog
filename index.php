630663c7ee46eb2314b814cb1578ca42efec15ff
AmirMohammad <amirmhaghighat@gmail.com>
2019-07-11 14:49:11 +0430

    add about,contact and services pages - add navbar.php

--- index.php
+++ index.php
@@ -15,75 +15,10 @@
     </div>
 
 
-    <header class="site-navbar py-3" role="banner">
+    <?php include('inc/navbar.php') ?>
 
-
-
-
-      <div class="container">
-        <div class="row align-items-center">
-
-          <div class="col-6 col-xl-2">
-            <h1 class="mb-0"><a href="<?php bloginfo('url')?>" class="text-black h2 mb-0">fotograp<span
-                  class="text-primary">.</span></a></h1>
-          </div>
-          <div class="col-10 col-md-8 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right text-lg-center" role="navigation">
-
-              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
-                <li class="active"><a href="index.html">Home</a></li>
-                <li class="has-children">
-                  <a href="single.html">Photography</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Nature</a></li>
-                    <li><a href="#">Portrait</a></li>
-                    <li><a href="#">Wedding</a></li>
-                    <li class="has-children">
-                      <a href="#">Sub Menu</a>
-                      <ul class="dropdown">
-                        <li><a href="#">Menu One</a></li>
-                        <li><a href="#">Menu Two</a></li>
-                        <li><a href="#">Menu Three</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-              </ul>
-            </nav>
-          </div>
-
-          <div class="col-6 col-xl-2 text-right">
-            <div class="d-none d-xl-inline-block">
-              <ul class="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0" data-class="social">
-                <li>
-                  <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                </li>
-                <li>
-                  <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                </li>
-                <li>
-                  <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                </li>
-                <li>
-                  <a href="#" class="pl-3 pr-3"><span class="icon-youtube-play"></span></a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#"
-                class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-          </div>
-
-        </div>
-      </div>
-
-    </header>
-
-    <div class="site-blocks-cover overlay inner-page-cover" style="background-image: url('<?php echo get_template_directory_uri() ?>/images/hero_bg_3.jpg');"
+    <div class="site-blocks-cover overlay inner-page-cover"
+      style="background-image: url('<?php echo get_template_directory_uri() ?>/images/hero_bg_3.jpg');"
       data-stellar-background-ratio="0.5">
       <div class="container">
         <div class="row align-items-center justify-content-center">
@@ -108,7 +43,8 @@
         <div class="row">
           <div class="col-md-6">
             <div class="site-block-half d-lg-flex">
-              <div class="image" style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_1.jpg');"></div>
+              <div class="image"
+                style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_1.jpg');"></div>
               <div class="text">
                 <h3>Nature Photography</h3>
                 <p>Sunt nesciunt repellat molestias vitae nostrum aliquid laudantium quo voluptatem provident voluptate
@@ -116,7 +52,8 @@
               </div>
             </div>
             <div class="site-block-half d-lg-flex">
-              <div class="image" style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_3.jpg');"></div>
+              <div class="image"
+                style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_3.jpg');"></div>
               <div class="text">
                 <h3>Portrait Photography</h3>
                 <p>Sunt nesciunt repellat molestias vitae nostrum aliquid laudantium quo voluptatem provident voluptate
@@ -126,7 +63,8 @@
           </div>
           <div class="col-md-6">
             <div class="site-block-half d-lg-flex">
-              <div class="image" style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_2.jpg');"></div>
+              <div class="image"
+                style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_2.jpg');"></div>
               <div class="text">
                 <h3>Wedding Photography</h3>
                 <p>Sunt nesciunt repellat molestias vitae nostrum aliquid laudantium quo voluptatem provident voluptate
@@ -134,7 +72,8 @@
               </div>
             </div>
             <div class="site-block-half d-lg-flex">
-              <div class="image" style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_4.jpg');"></div>
+              <div class="image"
+                style="background-image: url('<?php echo get_template_directory_uri() ?>/images/img_4.jpg');"></div>
               <div class="text">
                 <h3>Food & Drink Photography</h3>
                 <p>Sunt nesciunt repellat molestias vitae nostrum aliquid laudantium quo voluptatem provident voluptate
@@ -146,7 +85,8 @@
       </div>
     </div>
 
-    <div class="py-5 site-block-testimonial" style="background-image: url('<?php echo get_template_directory_uri() ?>/images/hero_bg_1.jpg');"
+    <div class="py-5 site-block-testimonial"
+      style="background-image: url('<?php echo get_template_directory_uri() ?>/images/hero_bg_1.jpg');"
       data-stellar-background-ratio="0.5">
       <div class="container">
         <div class="row block-13 mb-5">
@@ -258,8 +198,8 @@
       </div>
     </div>
 
-<?php get_footer(); ?>
-  
+    <?php get_footer(); ?>
+
 
 </body>
 
