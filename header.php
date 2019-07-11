238d8c3e58367819db6c103d89ddcdcbf0c1170a
AmirMohammad <amirmhaghighat@gmail.com>
2019-07-11 15:00:05 +0430

    add acomoon link address

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300i,400,700" rel="stylesheet">
-  <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/fonts/icomoon/style.css">
 
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/magnific-popup.css">
