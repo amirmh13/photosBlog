67385c4bc60bec3bee36f27f6a87883406e3e51a
AmirMohammad <amirmhaghighat@gmail.com>
2019-10-08 14:51:34 +0330

    add logo to navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -6,8 +6,8 @@
             <div class="col-6 col-xl-3">
                 <h1 class="mb-0">
                     <a href="<?php bloginfo('url')?>" class="text-white h2 mb-0">
-                        Tiles gallery
-                        <span class="text-primary">.</span>
+                        <img height="85px" width="85px" src="<?php echo get_template_directory_uri() ?>/images/logo.png"
+                            alt="Rasoulifar Art Gallery">
                     </a>
                 </h1>
             </div>
@@ -20,15 +20,18 @@
                         </li>
 
                         <li class="<?php if(is_page_template( 'services.php' )) echo 'active'?>">
-                            <a href="<?php bloginfo('url'); if (get_locale() == 'en_US') echo '/products'; else echo '/محصولات'; ?>">
-                            <?php _e('Products','photosBlog') ?>
+                            <a
+                                href="<?php bloginfo('url'); if (get_locale() == 'en_US') echo '/products'; else echo '/محصولات'; ?>">
+                                <?php _e('Products','photosBlog') ?>
                             </a>
                         </li>
                         <li class="<?php if(is_page_template( 'about.php' )) echo 'active'?>">
-                            <a href="<?php bloginfo('url'); if (get_locale() == 'en_US') echo '/about-us'; else echo '/درباره-ما';?>"><?php _e('About Us','photosBlog') ?></a>
+                            <a
+                                href="<?php bloginfo('url'); if (get_locale() == 'en_US') echo '/about-us'; else echo '/درباره-ما';?>"><?php _e('About Us','photosBlog') ?></a>
                         </li>
                         <li class="<?php if(is_page_template( 'contact.php' )) echo 'active'?>">
-                            <a href="<?php bloginfo('url'); if (get_locale() == 'en_US') echo '/contact-us'; else echo '/تماس-با-ما';?>"><?php _e('Contact Us','photosBlog') ?></a>
+                            <a
+                                href="<?php bloginfo('url'); if (get_locale() == 'en_US') echo '/contact-us'; else echo '/تماس-با-ما';?>"><?php _e('Contact Us','photosBlog') ?></a>
                         </li>
 
 
@@ -53,7 +56,13 @@
                             <a href="#" class="pl-3 pr-3"><span class="icon-telegram"></span></a>
                         </li>
                     </ul> -->
-                    <?php dynamic_sidebar('myPhoto') ?>
+                    <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block p-0">
+                        <li>
+                            <?php dynamic_sidebar('myPhoto') ?>
+                        </li>
+
+                    </ul>
+
                 </div>
 
                 <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;">
